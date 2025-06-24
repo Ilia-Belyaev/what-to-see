2418a8bbcbb5de39baf9dfbe163df153e01d6a02
Ilia <ilya.belyaev.95@list.ru>
2025-06-24 15:05:27 +0300

    .

--- src/components/my-list/my-list-hoc.tsx
+++ src/components/my-list/my-list-hoc.tsx
@@ -1,28 +1,28 @@
-// import { useEffect, useState } from 'react';
-// import { useAppDispatch, useAppSelector } from '../../hooks';
-// import { getFavoriteFilms } from '../../store/slices/api-actions';
-// import { getAllFavoriteFilms } from '../../store/slices/favorite-films/selectors';
+import { useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from '../../hooks';
+import { getFavoriteFilms } from '../../store/slices/api-actions';
+import { getAllFavoriteFilms } from '../../store/slices/favorite-films/selectors';
 // import MyList from '../../pages/my-list/my-list';
 
-// export const MyListHOC = () => {
-//   const dispatch = useAppDispatch();
-//   const [loading, setLoading] = useState(true);
-//   const favoriteFilms = useAppSelector(getAllFavoriteFilms);
+export const MyListHOC = () => {
+  const dispatch = useAppDispatch();
+  // const [loading, setLoading] = useState(true);
+  const favoriteFilms = useAppSelector(getAllFavoriteFilms);
 
-//   useEffect(() => {
-//     dispatch(getFavoriteFilms());
+  useEffect(() => {
+    dispatch(getFavoriteFilms());
 
-//   },[dispatch]);
+  },[dispatch]);
 
-//   useEffect(() => {
-//     if (favoriteFilms) {
-//       setLoading(false);
-//     }
-//   }, [favoriteFilms]);
+  useEffect(() => {
+    if (favoriteFilms) {
+      // setLoading(false);
+    }
+  }, [favoriteFilms]);
 
-//   if (loading) {
-//     return;
-//   }
+  // if (loading) {
+  //   return;
+  // }
 
-//   return <MyList films={favoriteFilms}/>;
-// };
+  // return <MyList films={favoriteFilms}/>;
+};
